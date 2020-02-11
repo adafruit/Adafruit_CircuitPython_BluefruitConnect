1d05783b401d7ac6d55025a3f79bba364a090160
caternuson <caternuson@gmail.com>
2020-02-10 16:43:17 -0800

    update examples

--- examples/bluefruitconnect_uart.py
+++ examples/bluefruitconnect_uart.py
@@ -1,27 +1,34 @@
-# Stream accelerometer data from your phone or tablet.
+# Basic example for using the BLE Connect UART
 # To use, start this program, and start the Adafruit Bluefruit LE Connect app.
-# Connect and go to the Controller screen. Turn on
-# STREAM SENSOR DATA -> Accelerometer to send data from the device's
-# accelerometer. See how it matches what this prints.
+# Connect, and then select UART
 
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.nordic import UARTService
 from adafruit_bluefruit_connect.packet import Packet
-# Only the packet classes that are imported will be known to Packet.
-from adafruit_bluefruit_connect.accelerometer_packet import AccelerometerPacket
 
 ble = BLERadio()
 uart_server = UARTService()
 advertisement = ProvideServicesAdvertisement(uart_server)
 
 while True:
+    print("WAITING...")
     # Advertise when not connected.
     ble.start_advertising(advertisement)
     while not ble.connected:
         pass
 
+    # Connected
+    ble.stop_advertising()
+    print("CONNECTED")
+
+    # Loop and read packets
     while ble.connected:
-        packet = Packet.from_stream(uart_server)
-        if isinstance(packet, AccelerometerPacket):
-            print(packet.x, packet.y, packet.z)
+        if uart_server.in_waiting:
+            raw_bytes = uart_server.read(uart_server.in_waiting)
+            text = raw_bytes.decode().strip()
+            print("raw bytes =", raw_bytes)
+            print("text =", text)
+
+    # Disconnected
+    print("DISCONNECTED")
