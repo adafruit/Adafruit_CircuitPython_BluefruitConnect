6ac287b753c42a4249039f65503fcddb739e8445
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:15 -0400

    Update version string

--- adafruit_bluefruit_connect/__init__.py
+++ adafruit_bluefruit_connect/__init__.py
@@ -17,5 +17,5 @@ boards that run `Adafruit CircuitPython firmware
 
 # imports
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_BluefruitConnect.git"
