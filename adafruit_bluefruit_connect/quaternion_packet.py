e093ababdbf8e7e91a1efe98eab37543e2669f01
hundredvisionsguy <cwinikka@gmail.com>
2023-04-26 11:22:04 -0700

    Adding type annotations to several packets
    We are still getting mypy complaints about the Liskov Principle,
    and quarternion due to trying to return the sum of a bytes and an
    int. Need to flag this as an issue.

--- adafruit_bluefruit_connect/quaternion_packet.py
+++ adafruit_bluefruit_connect/quaternion_packet.py
@@ -12,6 +12,8 @@ Bluefruit Connect App Quaternion data packet.
 
 """
 
+from __future__ import annotations
+
 import struct
 
 from ._xyz_packet import _XYZPacket
@@ -23,18 +25,18 @@ class QuaternionPacket(_XYZPacket):
 
     # Use _XYZPacket to handle x, y, z, and add w.
 
-    _FMT_PARSE = "<xxffffx"
-    PACKET_LENGTH = struct.calcsize(_FMT_PARSE)
+    _FMT_PARSE: str = "<xxffffx"
+    PACKET_LENGTH: int = struct.calcsize(_FMT_PARSE)
     # _FMT_CONSTRUCT doesn't include the trailing checksum byte.
-    _FMT_CONSTRUCT = "<2sffff"
-    _TYPE_HEADER = b"!Q"
+    _FMT_CONSTRUCT: str = "<2sffff"
+    _TYPE_HEADER: bytes = b"!Q"
 
-    def __init__(self, x, y, z, w):
+    def __init__(self, x: float, y: float, z: float, w: float) -> None:
         """Construct a QuaternionPacket from the given x, y, z, and w float values."""
         super().__init__(x, y, z)
         self._w = w
 
-    def to_bytes(self):
+    def to_bytes(self) -> bytes:
         """Return the bytes needed to send this packet."""
         partial_packet = struct.pack(
             self._FMT_CONSTRUCT, self._TYPE_HEADER, self._x, self._y, self._z, self._w
@@ -42,7 +44,7 @@ class QuaternionPacket(_XYZPacket):
         return partial_packet + self.checksum(partial_packet)
 
     @property
-    def w(self):
+    def w(self) -> float:
         """The w value."""
         return self._w
 
