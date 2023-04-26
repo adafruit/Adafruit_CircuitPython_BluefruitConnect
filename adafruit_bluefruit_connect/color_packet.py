24695d31534f381f278075d13c6704f55123180d
hundredvisionsguy <cwinikka@gmail.com>
2023-04-26 10:39:44 -0700

    Adding type hints to color_packet

--- adafruit_bluefruit_connect/color_packet.py
+++ adafruit_bluefruit_connect/color_packet.py
@@ -12,21 +12,27 @@ Bluefruit Connect App color data packet.
 
 """
 
+from __future__ import annotations
+
 import struct
 
 from .packet import Packet
 
+try:
+    from typing import Optional # adjust these as needed
+except ImportError:
+    pass
 
 class ColorPacket(Packet):
     """A packet containing an RGB color value."""
 
-    _FMT_PARSE = "<xx3Bx"
-    PACKET_LENGTH = struct.calcsize(_FMT_PARSE)
+    _FMT_PARSE: str = "<xx3Bx"
+    PACKET_LENGTH: int = struct.calcsize(_FMT_PARSE)
     # _FMT_CONSTRUCT doesn't include the trailing checksum byte.
-    _FMT_CONSTRUCT = "<2s3B"
-    _TYPE_HEADER = b"!C"
+    _FMT_CONSTRUCT: str = "<2s3B"
+    _TYPE_HEADER: bytes = b"!C"
 
-    def __init__(self, color):
+    def __init__(self, color: Optional[Packet]) -> None:
         """Construct a ColorPacket from a 3-element :class:`tuple` of RGB
         values, or from an int color value 0xRRGGBB.
 
@@ -41,14 +47,14 @@ class ColorPacket(Packet):
             raise ValueError("Color must be an integer 0xRRGGBB or a tuple(r,g,b)")
 
     @classmethod
-    def parse_private(cls, packet):
+    def parse_private(cls, packet: Optional[Packet]) -> Optional[Packet]:
         """Construct a ColorPacket from an incoming packet.
         Do not call this directly; call Packet.from_bytes() instead.
         pylint makes it difficult to call this method _parse(), hence the name.
         """
         return cls(struct.unpack(cls._FMT_PARSE, packet))
 
-    def to_bytes(self):
+    def to_bytes(self) -> bytes:
         """Return the bytes needed to send this packet."""
         partial_packet = struct.pack(
             self._FMT_CONSTRUCT, self._TYPE_HEADER, *self._color
@@ -56,7 +62,7 @@ class ColorPacket(Packet):
         return self.add_checksum(partial_packet)
 
     @property
-    def color(self):
+    def color(self) -> tuple:
         """A :class:`tuple` ``(red, green blue)`` representing the color the
         user chose in the BlueFruit Connect app."""
         return self._color
